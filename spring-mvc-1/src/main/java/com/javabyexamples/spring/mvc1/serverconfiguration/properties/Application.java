be97cf96fb6f42248df4df0f7d33c0639c55aacb
isaolmez <isaolmez@gmail.com>
2019-07-01 17:15:33 +0300

    Added mvc examples

---
+++ spring-mvc-1/src/main/java/com/javabyexamples/spring/mvc1/serverconfiguration/properties/Application.java
@@ -0,0 +1,12 @@
+package com.javabyexamples.spring.mvc1.serverconfiguration.properties;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder().main(Application.class).profiles("serverproperties").run(args);
+    }
+}
