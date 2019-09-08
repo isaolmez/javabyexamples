42d7e675e719af97dc72b3a478c4e0331aa36702
isaolmez <isaolmez@gmail.com>
2019-09-08 13:41:15 +0300

    Modify maven samples

--- maven-and-plugins/failsafe/src/test/java/com/javabyexamples/maven/plugins/failsafe/systemproperty/SystemPropertyIT.java
+++ maven-and-plugins/failsafe/src/test/java/com/javabyexamples/maven/plugins/failsafe/systemproperty/SystemPropertyIT.java
@@ -6,9 +6,17 @@ import org.junit.Test;
 public class SystemPropertyIT {
 
     @Test
-    public void shouldAccessSystemProperty() {
+    public void shouldAccessSystemProperty_WhenDefinedInSystemPropertyVariables() {
         Assert.assertEquals("Hello", System.getProperty("greet.english"));
+    }
+
+    @Test
+    public void shouldAccessSystemProperty_WhenDefinedInSystemProperties() {
         Assert.assertEquals("Hola", System.getProperty("greet.spanish"));
+    }
+
+    @Test
+    public void shouldAccessSystemProperty_WhenDefinedInSystemPropertiesFile() {
         Assert.assertEquals("Hallo", System.getProperty("greet.german"));
     }
 }
--- maven-and-plugins/property/src/test/java/com/javabyexamples/maven/plugins/property/SetPropertyTest.java
+++ maven-and-plugins/property/src/test/java/com/javabyexamples/maven/plugins/property/SetPropertyTest.java
@@ -3,10 +3,10 @@ package com.javabyexamples.maven.plugins.property;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class SampleTest {
+public class SetPropertyTest {
 
     @Test
     public void shouldRun() {
-        Assert.assertEquals("myValue", System.getProperty("my.custom.property"));
+        Assert.assertEquals("myValue", System.getProperty("myCustomProperty"));
     }
 }
