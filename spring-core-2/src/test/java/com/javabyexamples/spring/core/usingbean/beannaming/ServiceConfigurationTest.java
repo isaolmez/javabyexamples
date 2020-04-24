a5d0e446aedc4140a8bcd98a117de11046ca91db
isaolmez <isaolmez@gmail.com>
2020-04-24 15:42:56 +0300

    Update bean naming with @Bean

---
+++ spring-core-2/src/test/java/com/javabyexamples/spring/core/usingbean/beannaming/ServiceConfigurationTest.java
@@ -0,0 +1,35 @@
+package com.javabyexamples.spring.core.usingbean.beannaming;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class ServiceConfigurationTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    public void testBeanRegistration_WithMethodName() {
+        assertThat(context.containsBean("greetingService")).isTrue();
+    }
+
+    @Test
+    public void testBeanRegistration_WithBeanAnnotation() {
+        assertThat(context.containsBean("firstGreetingService")).isTrue();
+        assertThat(context.containsBean("theGreetingService")).isTrue();
+    }
+
+    @Test
+    public void testBeanRegistration_WithQualifierAnnotation() {
+        assertThat(context.containsBean("serviceWithQualifier")).isTrue();
+        assertThat(context.containsBean("qualifiedGreetingService")).isFalse();
+    }
+}
