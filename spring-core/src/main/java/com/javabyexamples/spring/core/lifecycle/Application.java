4f771c44047a2228a38103a5f17fd78462bc3536
isaolmez <isaolmez@gmail.com>
2020-04-27 11:41:18 +0300

    Update Spring version

--- spring-core/src/main/java/com/javabyexamples/spring/core/lifecycle/Application.java
+++ spring-core/src/main/java/com/javabyexamples/spring/core/lifecycle/Application.java
@@ -1,17 +1,12 @@
 package com.javabyexamples.spring.core.lifecycle;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @ComponentScan
 public class Application {
-
-    @Autowired
-    private Environment environment;
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
