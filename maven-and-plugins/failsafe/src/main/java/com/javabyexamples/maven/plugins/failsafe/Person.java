5104408099c53fed5f6bdf358b9e17997f7c0a8f
isaolmez <isaolmez@gmail.com>
2020-05-03 11:28:53 +0300

    Update failsafe project

--- maven-and-plugins/failsafe/src/main/java/com/javabyexamples/maven/plugins/failsafe/Person.java
+++ maven-and-plugins/failsafe/src/main/java/com/javabyexamples/maven/plugins/failsafe/Person.java
@@ -2,11 +2,11 @@ package com.javabyexamples.maven.plugins.failsafe;
 
 public class Person {
 
-    public String sayHello(){
+    public String sayHello() {
         return "Hello";
     }
 
-    public String sayBye(){
+    public String sayBye() {
         return "Bye";
     }
 }
