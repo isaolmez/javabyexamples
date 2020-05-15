ea328b5eb8da837e4409142a4878cf21fae880e5
isaolmez <isaolmez@gmail.com>
2020-05-15 15:00:59 +0300

    Update lock examples

--- java-and-concurrency/advanced/src/main/java/com/javabyexamples/java/concurrency/buildingblocks/synchronizers/lock/LockDetails.java
+++ java-and-concurrency/advanced/src/main/java/com/javabyexamples/java/concurrency/buildingblocks/synchronizers/lock/LockDetails.java
@@ -1,26 +1,80 @@
 package com.javabyexamples.java.concurrency.buildingblocks.synchronizers.lock;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class LockDetails {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         final LockDetails lockDetails = new LockDetails();
-        lockDetails.unlockWhenNotLocked();
-        lockDetails.lockMultipleTimesOnTheSameThread();
+//        lockDetails.lockUsage();
+//        lockDetails.lockReentrancy(5);
+//        lockDetails.unlockWhenNotLocked();
+
+        lockDetails.lockInterruptiblyUsage();
+        lockDetails.tryLockUsage();
     }
 
     private final ReentrantLock lock = new ReentrantLock();
+
+    public void lockUsage() {
+        lock.lock();
+        try {
+            doWork();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void lockReentrancy(int times) {
+        lock.lock();
+        System.out.println(Thread.currentThread().getName() + " acquired: " + lock.getHoldCount());
+        try {
+            if (times != 0) {
+                lockReentrancy(times - 1);
+            }
+        } finally {
+            lock.unlock();
+            System.out.println(Thread.currentThread().getName() + " released: " + lock.getHoldCount());
+        }
+    }
 
     public void unlockWhenNotLocked() {
         System.out.println("Currently held: " + lock.isHeldByCurrentThread());
         lock.unlock();
     }
 
-    public void lockMultipleTimesOnTheSameThread() {
-        lock.lock();
-        System.out.println("Acquired lock.");
-        lock.lock();
-        System.out.println("Acquired lock again, since it's reentrant.");
+
+    public void lockInterruptiblyUsage() throws InterruptedException {
+        lock.lockInterruptibly();
+        try {
+            doWork();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void tryLockUsage() {
+        if (lock.tryLock()) {
+            try {
+                doWork();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void tryLockWithTimeoutUsage() throws InterruptedException {
+        if (lock.tryLock(1, TimeUnit.SECONDS)) {
+            try {
+                doWork();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    private void doWork() {
+        System.out.println("Doing work.");
     }
 }
