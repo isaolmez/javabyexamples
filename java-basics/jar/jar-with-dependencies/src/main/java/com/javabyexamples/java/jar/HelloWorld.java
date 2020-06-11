d65d50f272226846e1c0152ef50c72642a5f16e1
isaolmez <isaolmez@gmail.com>
2020-06-11 14:44:53 +0300

    Add jar examples

---
+++ java-basics/jar/jar-with-dependencies/src/main/java/com/javabyexamples/java/jar/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.javabyexamples.java.jar;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.upperCase("Hello World"));
+    }
+}
